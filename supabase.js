cdd6a268ddee5daf19cdcf2a2a2670111dc4110c
Norivo <krzysiek.kwarta@gmail.com>
2025-07-20 01:00:14 +0200

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,2 +1,4 @@
-// Konfiguracja Supabase
-console.log('Supabase connected');
+const { createClient } = supabase;
+const supabaseUrl = 'https://xqlxotxeuadglvgdqral.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+const db = createClient(supabaseUrl, supabaseKey);
