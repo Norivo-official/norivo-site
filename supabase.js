c764c9195e3d87c8f665860f02bcbabb1eda797e
Norivo <krzysiek.kwarta@gmail.com>
2025-07-21 19:14:18 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const { createClient } = supabase;
-const supabaseUrl = 'https://xqlxotxeuadglvgdqral.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
-const db = createClient(supabaseUrl, supabaseKey);
